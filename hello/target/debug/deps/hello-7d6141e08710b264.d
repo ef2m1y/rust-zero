f52f4148a2993492d135d63c9da4e64868c4fe35
ef2m1y <shisotem01072002@gmail.com>
2023-01-24 23:17:28 +0900

    1.4

---
+++ hello/target/debug/deps/hello-7d6141e08710b264.d
@@ -0,0 +1,5 @@
+/home/ilu/rust-zero/hello/target/debug/deps/hello-7d6141e08710b264.rmeta: src/main.rs
+
+/home/ilu/rust-zero/hello/target/debug/deps/hello-7d6141e08710b264.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/deps/hello-ebd41dad16fed6bd.d
@@ -0,0 +1,5 @@
+/home/ilu/rust-zero/hello/target/debug/deps/hello-ebd41dad16fed6bd.rmeta: src/main.rs
+
+/home/ilu/rust-zero/hello/target/debug/deps/hello-ebd41dad16fed6bd.d: src/main.rs
+
+src/main.rs:
