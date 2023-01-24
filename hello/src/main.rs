496c9519b4fe51c8192457b7e329bcd5bab372c0
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 06:59:05 +0900

    2.1.7

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,10 @@
 fn main() {
-    let arr: [u32; 4] = [1, 2, 3, 4];
-    println!("{}, {}, {}, {}", arr[0], arr[1], arr[2], arr[3]);
+    let a: &str = " Hello";
+    // a += ", world!";
 
-    let s: &[u32] = &arr[1..3];
-    println!("s = {:?}, s[0] = {}", s, s[0]);
-
-    // arr[10] -> compile error (static analysis)
-    // s[10] -> panic (dynamic analysis)
-
-    let arr2: [u32; 5] = [3; 5];
-    println!("{:?}", arr2);
+    let mut b: String = a.to_string();
+    b += ", world!  ";
+    
+    let c: &str = b.trim();
+    println!("{c}");
 }
