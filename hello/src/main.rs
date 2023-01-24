796b3499d8399c9d7148fbca0073d8b6d1466058
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 04:18:39 +0900

    2.1.5

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let n: u8 = 0b0001_1000;
-    println!("n = {n}"); // 24
-    println!("{}", n << 2); // 96 = 0b0110_0000
-    println!("{}", n >> 2); // 6 = 0b0000_0110
+    let mut n: u64 = 100;
 
-    let m: i8 = -8;
-    println!("m = {m}"); // 0b1111_1000
-    println!("{}", m << 2); // -32 = 0b1110_0000
-    println!("{}", m >> 2); // -2 = 0b1111_1110
+    let a: &u64 = &n;
+    // *a = 200;
+    println!("*a = {}, addr = {:p}", *a, a);
+
+    let b: &mut u64 = &mut n;
+    *b = 300;
+    println!("*b = {}, addr = {:p}", *b, b);
+
+    println!("n = {n}, addr = {:p}", &n);
 }
