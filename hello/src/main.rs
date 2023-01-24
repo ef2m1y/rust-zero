264a0ac31a898a88f0bf755e1aca55c13e27de12
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 04:14:19 +0900

    2.1.4

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    println!("XOR");
-    println!("t^t: {}", xor(true, true));
-    println!("t^f: {}", xor(true, false));
-    println!("f^t: {}", xor(false, true));
-    println!("f^f: {}", xor(false, false));
-}
+    let n: u8 = 0b0001_1000;
+    println!("n = {n}"); // 24
+    println!("{}", n << 2); // 96 = 0b0110_0000
+    println!("{}", n >> 2); // 6 = 0b0000_0110
 
-fn xor(x: bool, y: bool) -> bool {
-    x ^ y
+    let m: i8 = -8;
+    println!("m = {m}"); // 0b1111_1000
+    println!("{}", m << 2); // -32 = 0b1110_0000
+    println!("{}", m >> 2); // -2 = 0b1111_1110
 }
