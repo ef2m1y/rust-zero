44b1ac32cbcdcb0200d68b0d450cf6398fe09445
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 18:54:39 +0900

    2.2.4

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,9 +1,16 @@
 fn main() {
-    // ラベルはforやwhileの前にも付けることが可能
-    'outer_loop: loop {
-        loop {
-            break 'outer_loop;
+    let v = [1, 2, 4, 5, 6, 8, 9, 11];
+    let mut sum = 0;
+    // v.iter(): 配列の要素への不変参照
+    for x in v.iter() {
+        if *x % 2 == 0 {
+            // continueもラベルを付けることが可能
+            continue;
         }
+        sum += *x;
     }
-    println!("escaped!");
+    println!("sum of odd numbers: {sum}");
+
+    // for, whileはbreakとともに値を返す事は出来ない
+    // -> loopと異なり必ずしもbreakで終了するとは限らないため
 }
