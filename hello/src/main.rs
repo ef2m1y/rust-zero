e1c1639a835f559b69d54b1d06b349b5c6124f52
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 13:56:10 +0900

    2.1.11

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,7 +1,28 @@
-fn main() {
-    let buf = Buffer::<128> { buf: [0; 128] };
+enum Option<T> {
+    Some(T),
+    None,
 }
 
-struct Buffer<const S: usize> {
-    buf: [u8; S],
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
 }
+
+fn main() {}
+
+// fn maybe_fail1() -> Option<u32> {
+//     // Processing something
+//     if success {
+//         Some(successful result)
+//     } else {
+//         None
+//     }
+// }
+//  fn maybe_fail2() -> Result<u32, &str> {
+//     // Processing something
+//     if success {
+//         Ok(successful result)
+//     } else {
+//         Err("error message")
+//     }
+//  }
