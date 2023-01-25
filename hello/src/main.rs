59ab202b05958c7b56a6715d1f86e1ac1dc2c022
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 18:42:23 +0900

    2.2.4

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    fn sumup_for(n: u64) -> u64 {
-        let mut sum: u64 = 0;
-        for i in 0..=n {
-            sum += i;
+    // ラベルはforやwhileの前にも付けることが可能
+    'outer_loop: loop {
+        loop {
+            break 'outer_loop;
         }
-        sum
     }
-
-    println!("{}", sumup_for(10));
-    println!("{}", sumup_for(100));
+    println!("escaped!");
 }
