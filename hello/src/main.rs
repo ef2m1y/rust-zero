305a55d5291a52d43241545fb46c1686ba352c7d
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 14:03:42 +0900

    2.1.12

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,28 +1,8 @@
-enum Option<T> {
-    Some(T),
-    None,
+fn main() {
+    let n: i32 = 100;
+    let m: i64 = n as i64;
+
+    let s: String = String::from("abcde");
+    let s: String = "abc".into();
+    let s: String = "abc".to_string();
 }
-
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
-
-fn main() {}
-
-// fn maybe_fail1() -> Option<u32> {
-//     // Processing something
-//     if success {
-//         Some(successful result)
-//     } else {
-//         None
-//     }
-// }
-//  fn maybe_fail2() -> Result<u32, &str> {
-//     // Processing something
-//     if success {
-//         Ok(successful result)
-//     } else {
-//         Err("error message")
-//     }
-//  }
