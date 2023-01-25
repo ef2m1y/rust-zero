db4d82cd7862569e8b90c321a73bb2b233f4f40b
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 18:22:12 +0900

    2.2.4

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,15 +1,7 @@
 fn main() {
-    fn sumup_loop(mut n: u64) -> u64 {
-        let mut sum: u64 = 0;
-        loop {
-            if n == 0 {
-                break sum;
-            }
-            sum += n;
-            n -= 1;
-        }
+    if loop {
+        break true;
+    } {
+        println!("break and return true");
     }
-
-    println!("{}", sumup_loop(10));
-    println!("{}", sumup_loop(100));
 }
