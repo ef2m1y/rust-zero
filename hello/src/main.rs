c55f9c96cb7fee1d300c320807774952465bf994
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 09:58:58 +0900

    2.1.10

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,19 +1,28 @@
-// enum DoW {
-//     Sunday,
-//     Monday,
-//     Tuesday,
-//     Wednesday,
-//     Thursay,
-//     Friday,
-//     Saturday,
-// }
-
 enum Storage {
     HDD { size: u32, rpm: u32 },
     SSD(u32),
 }
 
+struct PCSpec {
+    cpus: u16,
+    memory: u32,
+    storage: Storage,
+}
+
+struct Dim2(u32, u32);
+
 fn main() {
-    let hdd = Storage::HDD { size: 512, rpm: 7200 };
-    let ssd = Storage::SSD(512);
+    let spec = PCSpec {
+        cpus: 8,
+        memory: 16,
+        storage: Storage::SSD(1024),
+    };
+    println!("{}", spec.cpus);
+
+    let d2 = Dim2(10, 20);
+    println!("{}", d2.0);
+
+    let r = &spec;
+    println!("{}", (*r).cpus);
+    println!("{}", r.cpus); // auto dereference
 }
