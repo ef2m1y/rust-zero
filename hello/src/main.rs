4410b1bbacf778fce600bbe351c1fb6a8da8c189
ef2m1y <shisotem01072002@gmail.com>
2023-01-25 09:22:31 +0900

    2.1.9

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,17 @@
-fn main() {}
+fn main() {
+    // function pointer type
+    print_it(add, 60, 717);
+    print_it(mul, 25, 25);
+}
 
-fn func1() -> () {}
+fn print_it(f: fn(u32, u32) -> u32, a: u32, b: u32) {
+    println!("{}", f(a, b));
+}
 
-fn func2() {}
+fn add(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+fn mul(a: u32, b: u32) -> u32 {
+    a * b
+}
