3b8cd8ed224f4d0ad25fc6c05a386c2a12e4d44d
ef2m1y <shisotem01072002@gmail.com>
2023-01-26 22:15:25 +0900

    2.2.5

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,39 +1,13 @@
-enum Storage {
-    HDD { size: u32, rpm: u32 },
-    SSD(u32),
-}
-
-struct PCSpec {
-    cpus: u16,
-    memory: u32,
-    storage: Storage,
+fn maybe_fail() -> Option<u32> {
+    Some(100)
 }
 
 fn main() {
-    let spec = PCSpec {
-        cpus: 8,
-        memory: 16,
-        storage: Storage::SSD(1024),
-    };
+    if let Some(n) = maybe_fail() {
+        println!("{n}");
+    }
 
-    match &spec {
-        PCSpec {
-            storage: Storage::SSD(512),
-            ..
-        } => {
-            println!("512GiB SSD");
-        }
-        PCSpec {
-            cpus: 4 | 8,
-            memory: m,
-            storage: _,
-        } => {
-            println!("4 or 8 CPUs");
-            println!("{}GiB memory", *m);
-        }
-        PCSpec { memory: m, .. } if *m < 4 => {
-            println!("less than 4GiB memory")
-        }
-        _ => (),
+    while let Some(n) = maybe_fail() {
+        println!("{n}")
     }
 }
