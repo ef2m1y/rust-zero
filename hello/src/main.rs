ca55685eb3b2cca2db420c8bad0fc7c87a6d09a3
ef2m1y <shisotem01072002@gmail.com>
2023-01-26 22:49:44 +0900

    2.2.6

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,18 @@
-fn maybe_fail() -> Option<u32> {
-    Some(100)
+fn average(v: &[f32]) -> Option<f32> {
+    if v.is_empty() {
+        return None;
+    }
+
+    let mut sum = 0.0;
+    for x in v {
+        sum += x;
+    }
+
+    Some(sum / v.len() as f32)
 }
 
 fn main() {
-    if let Some(n) = maybe_fail() {
-        println!("{n}");
-    }
-
-    while let Some(n) = maybe_fail() {
-        println!("{n}")
-    }
+    let arr = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0];
+    let res = average(&arr[..]);
+    println!("{:?}", res);
 }
